bb71b139299cedb54ba57ce10faf41e2244dac3f
Xzector <85714981+Xzector@users.noreply.github.com>
2023-04-06 13:57:50 -0700

    Update and rename changelogs-library.txt to changelogs-library.lua

---
+++ changelogs-library.lua
@@ -0,0 +1,53 @@
+local n = setmetatable({ }, { __index = function(self, k) return game:GetService(k) end });
+local str_rep = string.rep;
+local replic_nm = 30;
+local change_logs = { lent = { } };
+
+local rconsolewarn = rconsolewarn
+local rconsolename = rconsolename
+local rconsoleclear = rconsoleclear
+
+change_logs.__index = change_logs;
+
+function change_logs.new(ac)
+    local self = setmetatable({}, change_logs);
+    
+    self.terminal_name = n.HttpService:GenerateGUID(false);
+    
+    if ac then
+        rconsolename(self.terminal_name);
+        do
+            rconsoleclear();
+        end;
+    end;
+    
+    do
+        return self;
+    end
+end;
+
+
+function change_logs:change_log(n)
+    local rep_str = str_rep('=', replic_nm);
+    do
+        if n.en then
+            do
+                rconsolewarn(rep_str .. ' ' .. n.nm .. ' ' ..  rep_str);
+            end;
+        end
+    end;
+    
+    do
+        return self;
+    end;
+end;
+
+function change_logs:txt(n)
+    table.insert(change_logs.lent, #change_logs.lent)
+    
+    rconsolewarn('['..#change_logs.lent..']' .. n.split .. n.txt .. n.split)
+
+    return self;
+end;
+
+return change_logs

8c715112eee803f568fd73b985211a1ac715f428
Xzector <85714981+Xzector@users.noreply.github.com>
2023-04-07 12:36:26 -0700

    Update changelogs-library.lua

--- changelogs-library.lua
+++ changelogs-library.lua
@@ -1,64 +1,63 @@
-local str_rep = string.rep;
-local replic_nm = 30;
-local change_logs = { lent = { } };
+local uuid = game.HttpService:GenerateGUID(true)
+local str = string.rep
+local change_logs = { lent = { } }
 
-local rconsolewarn = rconsolewarn
-local rconsolename = rconsolename
-local rconsoleclear = rconsoleclear
-local tins = table.insert
-local rconsoleerr = rconsoleerr
-local setmetatable = setmetatable
-
-change_logs.__index = change_logs;
+change_logs.__index = change_logs
 
 function change_logs.new(ac)
-    local self = setmetatable({}, change_logs);
-    
-    self.terminal_name = game.HttpService:GenerateGUID(false);
+    local self = setmetatable({ }, change_logs)
+
+    self.terminal_name = uuid
     
     if ac then
-        rconsolename(self.terminal_name);
+        rconsolename(self.terminal_name)
         
         do
-            rconsoleclear();
-        end;
-    end;
+            rconsoleclear()
+        end
+    end
     
     do
-        return self;
-    end
-end;
-
-local function nw(...)
-    do
-        rconsoleerr(...)
+        return self
     end
 end
 
-function change_logs:change_log(n)
-    local rep_str = str_rep('=', replic_nm);
+local function notify(...)
+    do
+        rconsolewarn(...)
+    end
+end
+
+function change_logs:change_log(n, rep)
+    local note = (str('=', rep)..' '..n.nm..' '..str('=', rep))
     
     do
         if n.en then
             do
-                nw(rep_str .. ' ' .. n.nm .. ' ' ..  rep_str)
-            end;
+                notify(note)
+            end
         end
-    end;
+    end
     
     do
-        return self;
-    end;
-end;
+        return self
+    end
+end
 
 function change_logs:txt(n)
-    do
-        tins(change_logs.lent, #change_logs.lent);
-    end;
+    local lento = change_logs.lent
     
-    rconsoleerr('['..#change_logs.lent..']' .. n.split .. n.txt);
-
-    return self;
-end;
+    table.insert(lento, #lento)
+    
+    do
+        local noti = (#lento..' '..n.txt)
+        
+        notify(noti)
+    end
+    
+    do
+        return self
+    end
+end
 
 return change_logs
